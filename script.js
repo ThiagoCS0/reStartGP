834ca475f882cbfe53a2bec1a8647d05c8771751
Thiago C Silva <tkreserva@gmail.com>
2023-12-13 19:16:05 -0300

    corrigindo diretorio das imagens 3

--- script.js
+++ script.js
@@ -6,20 +6,20 @@ var GP=[
         em hardware e armazenamento local limitado.</br>
         Ao buscar uma transformação digital, migraram para a Amazon Web Services (AWS) com a ajuda da
         SecureKloud Technologies.`,
-        img: "https://github.com/ThiagoCS0/reStartGP/tree/main/src/im-hist.jpg"
+        img: "./src/im-hist.jpg"
     },
     {
         titulo:"Motivo",
         descricao:`Com uma infra-estrutura local que gera muitos gastos e buscando melhorar a confiabilidade e a
         redundância de seus bancos de dados, viram na AWS a solução.`,
-        img: "https://github.com/ThiagoCS0/reStartGP/tree/main/src/im-moti.jpg"
+        img: "./src/im-moti.jpg"
     },
     {
         titulo:"Migração",
         descricao:`Após a migração para a AWS, a Panasonic Avionics pode coletar, analisar e armazenar dados de forma
         mais eficiente e em grande escala.</br>
         Essa migração envolveu 10 TB de dados para a nuvem.`,
-        img: "https://github.com/ThiagoCS0/reStartGP/tree/main/src/im-migr.jpg"
+        img: "./src/im-migr.jpg"
     },
     {
         titulo:"Benefícios",
@@ -27,7 +27,7 @@ var GP=[
         iniciativas cruciais para a empresa.</br>
         Uma redução de 78% nos custos de armazenamento e 86% com gastos em infra-estrutura, são alguns dos
         benefícios de trabalhar com a AWS, além de ficar livre das restrições de data centers locais.`,
-        img: "https://github.com/ThiagoCS0/reStartGP/tree/main/src/im-bene.jpg"
+        img: "./src/im-bene.jpg"
     },
     {
         titulo:"Segurança",
@@ -35,7 +35,7 @@ var GP=[
         conformidade com as rigorosas regulamentações do setor.</br>
         Protegendo dados sensíveis com a AWS, cultivaram maior confiança entre passageiros, fabricantes e
         parceiros aéreos.`,
-        img: "https://github.com/ThiagoCS0/reStartGP/tree/main/src/im-segu.jpg"
+        img: "./src/im-segu.jpg"
     },
     {
         titulo:"Tempo",
@@ -43,7 +43,7 @@ var GP=[
         atualizações.</br>
         Com entregas mais rápidas, alcançadas com nuvem, impulsionaram a inovação e aprimorando a eficiência
         operacional.`,
-        img: "https://github.com/ThiagoCS0/reStartGP/tree/main/src/im-temp.jpg"
+        img: "./src/im-temp.jpg"
     },
     {
         titulo:"Clientes",
@@ -51,13 +51,13 @@ var GP=[
         clientes.</br>
         Com uma redução de 20% do tempo de consulta e a segurança que só a AWS é capaz de entregar serviços
         com a excelência que os clientes merecem.`,
-        img: "https://github.com/ThiagoCS0/reStartGP/tree/main/src/im-clie.jpg"
+        img: "./src/im-clie.jpg"
     },
     {
         titulo:"Mais",
         descricao:`Conheça a história de sucesso atravez do link abaixo:</br></br>
         <a href="https://aws.amazon.com/pt/solutions/case-studies/panasonic-avionics-amazon-aurora-case-study/" title="História de sucesso da Panasonic!">https://aws.amazon.com/pt/solutions/case-studies/panasonic-avionics-amazon-aurora-case-study/</a></br>`,
-        img: "https://github.com/ThiagoCS0/reStartGP/tree/main/src/im-mais.jpg"
+        img: "./src/im-mais.jpg"
     }
 ];  
 
